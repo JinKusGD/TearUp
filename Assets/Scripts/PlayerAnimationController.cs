96e4b23ffb2db589ce0d3529a3eb82018766d836
이진규 <gdjinkus@gmail.com>
2026-06-25 21:48:39 +0900

    [애니메이션] 각 상태에 따른 애니메이션 추가

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -2,5 +2,49 @@
 
 public class PlayerAnimationController
 {
+    private readonly Animator _animator;
+    private PlayerState _currentState;
 
+    public PlayerAnimationController(Animator animator)
+    {
+        _animator = animator;
+    }
+
+    public void SetState(PlayerState newState)
+    {
+        if (newState == _currentState)
+        {
+            return;
+        }
+
+        _currentState = newState;
+
+        ResetAllState();
+
+        switch (_currentState)
+        {
+            case PlayerState.Walk:
+                _animator.SetBool("Walk", true);
+                break;
+            case PlayerState.Run:
+                _animator.SetBool("Run", true);
+                break;
+            case PlayerState.Jump:
+                _animator.SetTrigger("Jump");
+                break;
+            case PlayerState.Fall:
+                _animator.SetBool("Fall", true);
+                break;
+            case PlayerState.Land:
+                _animator.SetTrigger("Land");
+                break;
+        }
+    }
+
+    private void ResetAllState()
+    {
+        _animator.SetBool("Walk", false);
+        _animator.SetBool("Run", false);
+        _animator.SetBool("Fall", false);
+    }
 }
