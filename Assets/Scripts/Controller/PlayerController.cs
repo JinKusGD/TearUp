30cbb9316329eb8c2c666e316425bcc5075b0e1b
이진규 <gdjinkus@gmail.com>
2026-06-25 20:59:22 +0900

    [점프] 플레이어 점프/바닥 체크 추가

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,17 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerState
+{
+    Idle,
+    Walk,
+    Jump,
+    Rise,
+    Fall
+}
+
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private Rigidbody _playerRigidbody;
     [SerializeField] private Transform _cameraTransform;
 
+    [Header("State")]
+    [SerializeField] private PlayerState _playerState;
+
     [Header("MoveSettings")]
     [SerializeField] private Vector2 _playerInput;
     [SerializeField] private float _moveSpeed = 10.0f;
     [SerializeField] private float _turnSpeed = 30.0f;
+
+    [Header("JumpSettings")]
+    [SerializeField] private GroundCheck _groundCheck;
+    [SerializeField] private bool _isJumpPressed;
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private bool _isGrounded;
+
+    private PlayerAnimationController _playerAnimationController; 
 
     private void Awake()
     {
@@ -34,26 +56,128 @@ public class PlayerController : MonoBehaviour
 
             _cameraTransform = Camera.main.transform;
         }
+
+        if (_groundCheck == null)
+        {
+            _groundCheck = GetComponentInChildren<GroundCheck>();
+
+            if (_groundCheck == null)
+            {
+                Debug.LogError($"플레이어 GroundCheck 스크립트가 없습니다.");
+            }
+        }
+
+        if (!TryGetComponent(out Animator animator))
+        {
+            Debug.LogError($"플레이어 애니메이터가 없습니다.");
+            return;
+        }
+
+        _playerAnimationController = new PlayerAnimationController(animator);
     }
 
     private void OnEnable()
     {
         InputManager.Instance.BindPlayerMoveAction(OnMove);
+        InputManager.Instance.BindPlayerJumpAction(OnJump);
+        _groundCheck.BindGroundCheckAction(OnGroundCheck);
     }
+
+    private void Update()
+    {
+        if (_isGrounded)
+        {
+            UpdateGroundState();
+        }
+        else
+        {
+            UpdateAirState();
+        }
+
+        CurrentStateOnUpdate();
+    }
+
 
     private void FixedUpdate()
     {
         Move();
+
+        if (_isJumpPressed)
+        {
+            Jump();
+        }
     }
 
     private void OnDisable()
     {
         InputManager.Instance.UnBindPlayerMoveAction(OnMove);
+        InputManager.Instance.UnBindPlayerJumpAction(OnJump);
+        _groundCheck.UnBindGroundCheckAction();
+    }
+
+    private void UpdateGroundState()
+    {
+        if (_playerInput == Vector2.zero)
+        {
+            _playerState = PlayerState.Idle;
+        }
+        else
+        {
+            _playerState = PlayerState.Walk;
+        }
+    }
+
+    private void UpdateAirState()
+    {
+        float yVelocity = _playerRigidbody.linearVelocity.y;
+
+        if (yVelocity >= 0.1f)
+        {
+            _playerState = PlayerState.Rise;
+        }
+        else if (yVelocity <= -0.1f)
+        {
+            _playerState = PlayerState.Fall;
+        }
+    }
+    private void CurrentStateOnUpdate()
+    {
+        _playerAnimationController.SetState(_playerState);
+
+        switch (_playerState)
+        {
+            case PlayerState.Idle:
+                IdleState();
+                break;
+            case PlayerState.Walk:
+                WalkState();
+                break;
+            case PlayerState.Jump:
+                JumpState();
+                break;
+            case PlayerState.Rise:
+                RiseState();
+                break;
+            case PlayerState.Fall:
+                FallState();
+                break;
+        }
     }
 
     private void OnMove(InputAction.CallbackContext context)
     {
         _playerInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        float jumpInput = context.ReadValue<float>();
+
+        _isJumpPressed = (jumpInput != 0);
+    }
+    private void OnGroundCheck(bool isGrounded)
+    {
+        _isGrounded = isGrounded;
     }
 
     private void Move()
@@ -78,5 +202,53 @@ public class PlayerController : MonoBehaviour
             _playerRigidbody.MoveRotation(Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed * Time.fixedDeltaTime));
             _playerRigidbody.MovePosition(_playerRigidbody.position + targetPosition);
         }
+    }
+
+    private void Jump()
+    {
+        if (!_isGrounded)
+        {
+            return;
+        }
+
+        _isGrounded = false;
+
+        Vector3 velocity = _playerRigidbody.linearVelocity;
+        velocity.y = 0f;
+        _playerRigidbody.linearVelocity = velocity;
+
+        _playerAnimationController.SetState(PlayerState.Jump);
+        _playerRigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void IdleState()
+    {
+        Debug.Log("Idle 상태");
+    }
+
+    private void WalkState()
+    {
+        Debug.Log("Move 상태");
+    }
+
+    private void JumpState()
+    {
+        Debug.Log("jump 상태");
+    }
+
+    private void RiseState()
+    {
+        Debug.Log("Rise 상태");
+        //_playerRigidbody.linearVelocity += Vector3.up * Physics.gravity.y * (_fallMultiplier - 1) * Time.fixedDeltaTime;
+
+        // 예: _animator.SetBool("IsFalling", true);
+    }
+
+    private void FallState()
+    {
+        Debug.Log("Fall 상태");
+        //_playerRigidbody.linearVelocity += Vector3.up * Physics.gravity.y * (_fallMultiplier - 1) * Time.fixedDeltaTime;
+
+        // 예: _animator.SetBool("IsFalling", true);
     }
 }
---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private Action<bool> _groundTriggerEvent;
+
+    public void BindGroundCheckAction(Action<bool> callback)
+    {
+        _groundTriggerEvent = callback;
+    }
+
+    public void UnBindGroundCheckAction()
+    {
+        _groundTriggerEvent = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Ground"))
+        {
+            return;
+        }
+
+        _groundTriggerEvent.Invoke(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Ground"))
+        {
+            return;
+        }
+
+        _groundTriggerEvent.Invoke(false);
+    }
+}
