318b81f73cf30a6f87103504cb08e44ba441a6df
이진규 <gdjinkus@gmail.com>
2026-07-05 16:13:17 +0900

    [인벤토리] 인벤토리 UI를 열고 닫는 메서드 추가

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,16 +1,32 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform _canvas;
+    [SerializeField] private GameObject _inventoryUI;
+
+    private GameObject _createdInventoryUI;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (_createdInventoryUI == null)
+            {
+                _createdInventoryUI = CreateInventory();
+                _createdInventoryUI.SetActive(true);
+                return;
+            }
+
+            bool isActive = _createdInventoryUI.activeSelf;
+            _createdInventoryUI.SetActive(!isActive);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject CreateInventory()
     {
-        
+        GameObject InventoryUi = Instantiate(_inventoryUI, _canvas);
+
+        return InventoryUi;
     }
 }
